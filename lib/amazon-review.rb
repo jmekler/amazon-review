20d9280d8b4c2e7f7a314a8bc5e991432c846e16
Jeff Mekler <jmekler@gmail.com>
2014-09-09 11:38:14 -0400

    Changes to manage 503 timeouts and rate limiting

--- lib/amazon-review.rb
+++ lib/amazon-review.rb
@@ -5,10 +5,12 @@ module AmazonReview
 
   def self.find_reviews(asin)
     reviews = []
+    delay = 0.5
     page = 1
     
     # iterate through the pages of reviews
-    begin  
+
+    begin
       url = "http://www.amazon.com/product-reviews/#{asin}/?ie=UTF8&showViewpoints=0&pageNumber=#{page}&sortBy=bySubmissionDateAscending"
       doc = Nokogiri::HTML(open(url))
       
@@ -17,9 +19,17 @@ module AmazonReview
       doc.css("#productReviews td > a[name]").each do |review_html|
         reviews << Review.new(review_html)
         new_reviews += 1
-      end 
-      
+      end       
+      # go to next page
       page += 1
+      
+      # delay to prevent 503 errors
+      delay = [0, delay - 0.1].max # decrease delay
+      sleep delay
+      
+    rescue Exception => e # error while parsing (likely a 503)      
+      delay += 0.5 # increase delay
+    
     end until new_reviews == 0
     
     reviews  
